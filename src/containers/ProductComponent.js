8ab51aa864b637732e9954d281f706a64e259f5f
jugosack <jugoslav_ackoski@yahoo.com>
2023-03-17 23:41:49 +0100

    Add folders containers, images, redux

---
+++ src/containers/ProductComponent.js
@@ -0,0 +1,52 @@
+// eslint-disable-next-line import/no-duplicates
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+// eslint-disable-next-line import/no-duplicates
+import { useState } from 'react';
+import '../App.css';
+
+const ProductComponent = () => {
+  const products = useSelector((state) => state.allProducts.products);
+  const [query, setQuery] = useState(' ');
+
+  const renderList = products.filter((product) => product.category.toLowerCase().includes(query))
+    .map((product) => {
+      const {
+        id, title, image, price, category,
+      } = product;
+      return (
+        <div className="four wide column" key={id}>
+          <Link to={`/product/${id}`}>
+            <div className="ui link cards">
+              <div className="card">
+                <div className="image">
+                  <img src={image} alt={title} />
+                </div>
+                <div className="content">
+                  <div className="header">{title}</div>
+                  <div className="meta price">
+                    $
+                    {price}
+                  </div>
+                  <div className="meta">{category}</div>
+                </div>
+              </div>
+            </div>
+          </Link>
+        </div>
+      );
+    });
+  return (
+    <>
+      <div className="inputfilter">
+        <h1 className="filtertext">search by category</h1>
+        <input className="filterbar" placeholder="woman, man, jewerly..." onChange={(e) => setQuery(e.target.value)} type="text" />
+      </div>
+      <br />
+      {renderList}
+    </>
+  );
+};
+
+export default ProductComponent;
