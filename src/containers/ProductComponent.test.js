dca00e95cd0fab03caecb8606cc8e4104834f479
jugosack <jugoslav_ackoski@yahoo.com>
2023-03-18 15:59:19 +0100

    Cleaning project files and removing tests

--- src/containers/ProductComponent.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import ProductComponent from './ProductComponent';
-
-test('renders placeholder', () => {
-  render(<ProductComponent />);
-  const placeholder = screen.getByPlaceholderText('woman, man, jewerly...');
-  expect(placeholder).toBeInTheDocument();
-});
